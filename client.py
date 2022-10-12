c6f1312b20986ddde5e6c393b01aefc025cb7513
Balaram <webbdays@gmail.com>
2022-10-12 12:22:56 +0530

    updated the functions for the correct behaviour as a part of task-1

--- client.py
+++ client.py
@@ -31,29 +31,31 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
+    """ ------------- Update this function ------------- """ #updated
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    """ ------------- Update this function ------------- """  #updated
+    return price_a/price_b
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
-    for _ in iter(range(N)):
+    for _ in  range(N):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
-        """ ----------- Update to get the ratio --------------- """
+        print(quotes)
+        """ ----------- Update to get the ratio --------------- """ #updated
+        stock_prices={}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            stock_prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-
-        print("Ratio %s" % getRatio(price, price))
+        
+        print("Ratio %s" % getRatio(stock_prices["ABC"], stock_prices["DEF"]))
